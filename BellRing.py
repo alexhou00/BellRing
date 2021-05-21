26fa0f2a99f7e88a63e219289550df4d6e102748
alexhou00 <84550223+alexhou00@users.noreply.github.com>
2021-05-21 09:23:55 +0800

    Add files via upload

--- BellRing.py
+++ BellRing.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 mixer.init()
 
-mixer.music.load("bell_LKJH.mp3")
+mixer.music.load("./bell_LKJH.mp3")
 
 times = (( 7,30), ( 8,15), ( 8,25), ( 9,10), ( 9,20), (10, 5), (10,15), (11, 0), (11,10), (11,55),
          (12, 0), (12,20), (12,30), (13,10), (13,15), (14, 0), (14,10), (14,55), (15,10), (15,55))
